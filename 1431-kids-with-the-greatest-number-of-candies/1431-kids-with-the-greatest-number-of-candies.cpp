c326e7f3812d88bc08d7d390d90d4490761e2e12
jujeong <44399938+jujeong@users.noreply.github.com>
2022-02-13 21:28:39 +0900

    Time: 5 ms (34.23%), Space: 8.9 MB (83.50%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        vector<bool> res;
+        int max = *max_element(candies.begin(), candies.end());
+        for(int i = 0; i < candies.size(); i++)
+        {
+            if(candies[i] + extraCandies < max)
+            {
+                res.push_back(false);
+            }
+            else
+            {
+                res.push_back(true);
+            }
+        }
+        return res;
+    }
+};
